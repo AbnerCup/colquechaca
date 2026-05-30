010411bd4ca02c96b9e97c4737ed85d17a8910c0
AbnerCup <abner.copa.2020@gmail.com>
2026-05-30 11:08:42 -0400

    Routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TransaccionController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Vista principal
+Route::get('/', [TransaccionController::class, 'index'])->name('transacciones.index');
+
+// Rutas
+Route::get('/transacciones/data', [TransaccionController::class, 'getData'])->name('transacciones.data');
+Route::post('/transacciones', [TransaccionController::class, 'store'])->name('transacciones.store');
+Route::put('/transacciones/{id}', [TransaccionController::class, 'update'])->name('transacciones.update');
+Route::delete('/transacciones/{id}', [TransaccionController::class, 'destroy'])->name('transacciones.destroy');
+Route::get('/categorias-por-tipo/{tipo}', [TransaccionController::class, 'getCategoriasPorTipo'])->name('categorias.por-tipo');
